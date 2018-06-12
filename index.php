ae5211daf6fa9e0c49785e637fb1e34f1f798d2b
Michael Hanselmann <hansmi@vshn.ch>
2018-06-12 17:41:07 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+<h1>Hello World</h1>
+<p>Running on <code><?php echo htmlspecialchars(gethostname()); ?></code>.</p>
+</body>
+</html>
+<!-- vim: set sw=2 sts=2 et : -->
